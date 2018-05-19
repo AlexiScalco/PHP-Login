edbf80de3ff4a79787ee73b0f500e26b7c2c7347
AlexiScalco <39425210+AlexiScalco@users.noreply.github.com>
2018-05-18 23:06:21 -0400

    Index

---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<title>PHP Login</title>
+    
+    
+    <!-- UIkit CSS -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-rc.2/css/uikit.min.css" />
+</head>
+<body>
+    
+<div class="uk-section uk-container">
+  		<div class="uk-grid uk-child-width-1-3@s uk-child-width-1-1" uk-grid>
+			<form class="uk-form-stacked js-login">
+
+			    <div class="uk-margin">
+			        <label class="uk-form-label" for="form-stacked-text">Email</label>
+			        <div class="uk-form-controls">
+			            <input class="uk-input" id="form-stacked-text" type="email" required='required' placeholder="email@email.com">
+			        </div>
+			    </div>
+
+			    <div class="uk-margin">
+			        <label class="uk-form-label" for="form-stacked-text">Password</label>
+			        <div class="uk-form-controls">
+			            <input class="uk-input" id="form-stacked-text" type="password" required='required' placeholder="Your Password">
+			        </div>
+			    </div>
+
+			    <div class="uk-margin">
+			        <button class="uk-button uk-button-default" type="submit">Login</button>
+			    </div>
+
+			</form>
+  		</div>
+  	</div>    
+    
+    
+    
+    
+    
+<!--JQUERY-->
+<script src="https://code.jquery.com/jquery-3.3.1.js" integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60=" crossorigin="anonymous"></script>    
+    
+    
+    <!-- UIkit JS -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-rc.2/js/uikit.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-rc.2/js/uikit-icons.min.js"></script>
+</body>
+</html>
+
+
+
